2a28f536abab31d940d26b0e44ebe73c2628567d
Larrea <gorkalarreamontoya@gmail.com>
2022-11-13 19:16:27 +0100

    Commit 1: Creados Modelos

--- PizzeriaMammaMia/models.py
+++ PizzeriaMammaMia/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingrediente(models.Model):
+    nombre= models.CharField(max_length=50)
+    def __str__(self):
+        return f"id={self.id}, nombre={self.nombre}"
+    descripcion=models.CharField(max_length=1024) # o más incluso
+
+
+
+class Masa(models.Model):
+    nombre= models.CharField(max_length=50)
+    precio= models.IntegerField(default=1)
+    def __str__(self):
+        return f"id={self.id}, nombre={self.nombre}"
+    descripcion=models.CharField(max_length=1024) # o más incluso
+
+
+class Pizza(models.Model):
+    nombre= models.CharField(max_length=50)
+    def __str__(self):
+        return f"id={self.id}, nombre={self.nombre}"
+    descripcion=models.CharField(max_length=1024) # o más incluso
+    #Relacion many-to-many con los ingredientes
+    ingredientes = models.ManyToManyField(Ingrediente)
+    #Relacion one-to-many con las masas
+    masa = models.ForeignKey(Masa, on_delete=models.CASCADE)
+    precio = models.IntegerField(default=0)
+
--- PizzeriaMammaMia/views.py
+++ PizzeriaMammaMia/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import Departamento, Habilidad, Empleado
 
 # Create your views here.
+
+def 
