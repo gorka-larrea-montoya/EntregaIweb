ed092b85a99dcc18639ceb044148cc742b877c3e
Larrea <gorkalarreamontoya@gmail.com>
2022-12-13 03:09:32 +0100

    Progreso adecuadamente excepto con JS

--- PizzeriaMammaMia/urls.py
+++ PizzeriaMammaMia/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('pizza', views.PizzaListView.as_view(), name='index_pizzas'),
     path('pizza/<int:pk>',views.PizzaDetailView.as_view(), name='show_pizza'),
-    path('ingrediente', views.index_ingredientes,name='index_ingredientes'),
+    path('ingrediente', views.IngredienteListView.as_view(),name='index_ingredientes'),
     path('ingrediente/<int:ingrediente_id>', views.show_ingrediente,name='show_ingrediente'),
-    path('masa', views.index_masas,name='index_masas'),
-    path('masa/<int:pk>', views.MasaDetailView.as_view(),name='show_masa'),
+    path('masa', views.MasaListView.as_view(),name='index_masas'),
+    path('masa/<int:masa_id>', views.show_masa,name='show_masa'),
 
 ]
--- PizzeriaMammaMia/views.py
+++ PizzeriaMammaMia/views.py
@@ -30,35 +30,42 @@ class PizzaDetailView(DetailView):
 #    context = {'pizza' : pizza , 'masa': masa, 'ingredientes' : ingredientes}
 #    return render(request,'pizza.html',context)
 
-#def index_pizzas(request):
- #   pizzas = get_list_or_404(Pizza.objects.order_by('nombre'))
-  #  context = {'lista_pizzas' : pizzas}
-   # return render(request, 'pizzas.html', context)
-
 class PizzaListView(ListView):
     model = Pizza
     template_name = 'pizzas.html'
     queryset = Pizza.objects.order_by('id')
 
-#def show_masa(request,masa_id):
-#    masa = get_object_or_404(Masa,pk=masa_id)
-#    pizzas = get_list_or_404(Pizza,masa=masa_id)
-#    context = {'masa' : masa, 'pizzas' : pizzas}
-#    return render(request, 'masa.html', context)
+#def index_pizzas(request):
+ #   pizzas = get_list_or_404(Pizza.objects.order_by('nombre'))
+  #  context = {'lista_pizzas' : pizzas}
+   # return render(request, 'pizzas.html', context)
 
-class MasaDetailView(DetailView):
-    model = Masa
-    template_name = 'masa.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super(MasaDetailView,self).get_context_data(**kwargs)
-        #TODO pizzas = get_list_or_404(Pizza,masa)
-        return context
+#class MasaDetailView(DetailView):
+#    model = Masa
+#    template_name = 'masa.html'
+#    
+#    def get_context_data(self, **kwargs):
+#        context = super(MasaDetailView,self).get_context_data(**kwargs)
+#        #TODO pizzas = get_list_or_404(Pizza,masa)
+#        return context
 
-def index_masas(request):
-    masas = get_list_or_404(Masa.objects.order_by('nombre'))
-    context = {'masas' : masas}
-    return render(request, 'masas.html', context)
+def show_masa(request,masa_id):
+    masa = get_object_or_404(Masa,pk=masa_id)
+    pizzas = get_list_or_404(Pizza,masa=masa_id)
+    context = {'masa' : masa, 'pizzas' : pizzas}
+    return render(request, 'masa.html', context)
+
+class MasaListView(ListView):
+    model : Masa
+    template_name = "masas.html"
+    queryset = Masa.objects.order_by('id')
+
+#def index_masas(request):
+#    masas = get_list_or_404(Masa.objects.order_by('nombre'))
+#    context = {'masas' : masas}
+#    return render(request, 'masas.html', context)
+
+
 
 def show_ingrediente(request,ingrediente_id):
     ingrediente = get_object_or_404(Ingrediente,pk=ingrediente_id)
@@ -66,8 +73,13 @@ def show_ingrediente(request,ingrediente_id):
     context = {'ingrediente' : ingrediente, 'pizzas' : pizzas}
     return render(request, 'ingrediente.html', context)
         
-def index_ingredientes(request):
-    ingredientes = get_list_or_404(Ingrediente.objects.order_by('nombre'))
-    context = {'ingredientes' : ingredientes}
-    return render(request, 'ingredientes.html', context)
+class IngredienteListView(ListView):
+    model : Ingrediente
+    template_name = "ingredientes.html"
+    queryset = Ingrediente.objects.order_by('id')     
+
+#def index_ingredientes(request):
+#    ingredientes = get_list_or_404(Ingrediente.objects.order_by('nombre'))
+#    context = {'ingredientes' : ingredientes}
+#       return render(request, 'ingredientes.html', context)
 
